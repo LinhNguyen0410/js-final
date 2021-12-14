ba98ae20863c2f9e1a0f2ba22495f34f26045afb
Linh Nguyen <linhgbale0410@gmail.com>
2021-12-14 21:55:16 +0700

    search and handled pagination for home page

--- js/utils/common.js
+++ js/utils/common.js
@@ -5,3 +5,9 @@ export function setTextContent(parent, selector, text) {
   const element = parent.querySelector(selector);
   if (element) element.textContent = text;
 }
+
+export function textTruncate(text, maxLength) {
+  if (text.length <= maxLength) return text;
+
+  return `${text.slice(0, maxLength - 1)}…`;
+}
